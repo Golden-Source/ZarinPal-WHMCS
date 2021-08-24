0d5ec9358c5939092d81634d5c03ebfc8b7cc90c
GoldenSource <50501111+Golden-Source@users.noreply.github.com>
2021-08-24 11:13:15 +0430

    Create copyright.php

---
+++ modules/addons/ZarinpalAddon/include/templates/admin/copyright.php
@@ -0,0 +1,5 @@
+<div class="copyright">
+    (Version: <?= $vars['version']; ?>)
+    <br>
+    Design and development by (<a href="mailto:matiniamirhossein@gmail.com" target="_blank">Amirhossein Matini</a>) and (<a href="https://goldensource.ir" target="_blank">GoldenSource</a>) Team
+</div>
